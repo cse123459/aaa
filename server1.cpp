81313ab0c6dc12ecccc30cab9f28c47ad7e8822f
cse123459 <57055461+cse123459@users.noreply.github.com>
2019-10-27 10:44:16 +0530

    Add files via upload

---
+++ server1.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    int welcome, new_soc, fd, n;
+    char buffer[1024], fname[50];
+    struct sockaddr_in addr;
+
+    welcome = socket(AF_INET, SOCK_STREAM, 0);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(argv[1]));
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    bind(welcome, (struct sockaddr *) &addr, sizeof(addr));
+    printf("\nServer is Online");
+    /*  listen for connections from the socket */
+    listen(welcome, 5);
+    /*  accept a connection, we get a file descriptor */
+    new_soc = accept(welcome, NULL, NULL);
+
+    /*  receive the filename */
+    recv(new_soc, fname, 50, 0);
+    printf("\nRequesting for file: %s\n", fname);
+
+    /*  open the file and send its contents */
+    fd = open(fname, O_RDONLY);
+
+    if (fd < 0)
+        send(new_soc, "\nFile not found\n", 15, 0);
+    else
+	while((n=read(fd, buffer, 1023))>0)
+	{
+	    printf("%s", buffer);
+            send(new_soc, buffer, n, 0);
+	    bzero(buffer, 1024);
+	}
+
+    printf("\nRequest sent\n");
+    close(fd);
+
+    return 0;
+}
+
